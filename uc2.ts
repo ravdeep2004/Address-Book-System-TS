7335df9f127ef520bf98db2a5e6e0f19abef32aa
Ravdeep Singh <ravdeep2004@gmail.com>
2025-12-17 15:38:25 +0530

    feat:[Ravdeep] - Changes did to code(fixes)
    Removed Contact class and related address book functionality.

--- uc2.ts
+++ uc2.ts
@@ -1,23 +1,4 @@
-class Contact {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public address: string,
-    public city: string,
-    public state: string,
-    public zip: string,
-    public phone: string,
-    public email: string
-  ) {}
-}
 
-class AddressBook {
-  contacts: Contact[] = [];
-
-  addContact(contact: Contact): void {
-    this.contacts.push(contact);
-  }
-  
   editContact(firstName: string, newCity: string): void {
     const person = this.contacts.find(c => c.firstName === firstName);
 
@@ -26,13 +7,3 @@ class AddressBook {
     }
   }
 }
-
-
-const addressBook = new AddressBook();
-
-addressBook.addContact(
-  new Contact("Ravdeep", "Singh", "Delhi", "Delhi", "DL", "110001", "9999", "r@gmail.com")
-);
-
-addressBook.editContact("Ravdeep", "Mumbai");
-console.log(addressBook.contacts);
