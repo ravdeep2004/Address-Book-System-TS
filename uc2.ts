42fcf2fec6b35a454864bc8d92edd2f1a268de05
Ravdeep Singh <ravdeep2004@gmail.com>
2025-12-17 13:39:02 +0530

    feat:[Ravdeep] - Ability to add a new Contact to Address Book

---
+++ uc2.ts
@@ -0,0 +1,39 @@
+class Contact {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public address: string,
+    public city: string,
+    public state: string,
+    public zip: string,
+    public phone: string,
+    public email: string
+  ) {}
+}
+
+class AddressBook {
+  contacts: Contact[] = [];
+
+  addContact(contact: Contact): void {
+    this.contacts.push(contact);
+  }
+
+  // ✅ UC-2 METHOD (INSIDE CLASS)
+  editContact(firstName: string, newCity: string): void {
+    const person = this.contacts.find(c => c.firstName === firstName);
+
+    if (person) {
+      person.city = newCity;
+    }
+  }
+}
+
+
+const addressBook = new AddressBook();
+
+addressBook.addContact(
+  new Contact("Ravdeep", "Singh", "Delhi", "Delhi", "DL", "110001", "9999", "r@gmail.com")
+);
+
+addressBook.editContact("Ravdeep", "Mumbai");
+console.log(addressBook.contacts);
