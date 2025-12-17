7dd057bceeea989a8c47a1a732009a1fb9182eb3
Ravdeep Singh <ravdeep2004@gmail.com>
2025-12-17 15:27:02 +0530

    feat:[Ravdeep] - Ability to edit a contact

--- uc2.ts
+++
@@ -1,9 +0,0 @@
-
-  editContact(firstName: string, newCity: string): void {
-    const person = this.contacts.find(c => c.firstName === firstName);
-
-    if (person) {
-      person.city = newCity;
-    }
-  }
-}
